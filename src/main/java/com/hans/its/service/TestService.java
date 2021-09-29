5b0365dc81a3649f04b250de1cdc033e00f1e9fd
hans <crazyun2005@naver.com>
2021-09-29 10:39:54 +0900

    CRUD in progress

--- src/main/java/com/hans/its/service/TestService.java
+++ src/main/java/com/hans/its/service/TestService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @Slf4j
@@ -14,10 +15,18 @@ public class TestService {
 
     private final TestRepository testRepository;
 
-    public Flux<Table1> getRows() {
+    public Flux<Table1> getAllRows() {
         Flux<Table1> allRows = testRepository.findAll();
 //        List<Table1> blocked = allRows.collectList().block();
 //        blocked.forEach(item -> log.info("row : {}", item));
         return allRows;
+    }
+
+    public Mono<Table1> getRowById(long id) {
+        return testRepository.findById(id);
+    }
+
+    public Mono<Table1> createRow(Table1 table1){
+        return testRepository.save(table1);
     }
 }
--- src/main/java/com/hans/its/web/TestController.java
+++ src/main/java/com/hans/its/web/TestController.java
@@ -4,10 +4,8 @@ import com.hans.its.model.Table1;
 import com.hans.its.repository.TestRepository;
 import com.hans.its.service.TestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -17,13 +15,13 @@ import reactor.core.publisher.Mono;
 public class TestController {
     private final TestService testService;
 
-    @GetMapping("")
+    @GetMapping("/all")
     public Flux<Table1> getDto() {
-        return testService.getRows();
+        return testService.getAllRows();
     }
 
-    @PostMapping("/new")
-    public Mono<Object> createDto() {
-        return null;
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<Table1>> getRowById(@PathVariable long id) {
+        return testService.getRowById(id).map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
